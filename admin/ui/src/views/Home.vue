69575edb11d9b60eba7c3d747104b14337bf0753
Tyler Auerbeck <tauerbec@redhat.com>
2021-05-10 16:28:36 -0400

    Cleanup helm chart branch

--- admin/ui/src/views/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1>Poolboy</h1>
-  <p><router-link to="/resourcepools">ResourcePools</router-link></p>
-  <p><router-link to="/resourceclaims">ResourceClaims</router-link></p>
-  <p><router-link to="/resourcehandles">ResourceHandles</router-link></p>
-  <p><router-link to="/resourceproviders">ResourceProviders</router-link></p>
-</template>
